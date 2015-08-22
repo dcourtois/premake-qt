10f00fa4ace3c3b5b859a664654423815137b407
Damien Courtois <damien.courtois@gmail.com>
2015-08-22 14:39:53 +0200

    updated Qt module to use the _preload machanism

--- _manifest.lua
+++ _manifest.lua
@@ -1,4 +1,5 @@
 
 return {
+	"_preload.lua",
 	"qt.lua"
 }
---
+++ _preload.lua
@@ -0,0 +1,90 @@
+
+--
+-- Set the path where Qt is installed
+--
+premake.api.register {
+	name = "qtpath",
+	scope = "config",
+	kind = "path"
+}
+
+--
+-- Set the prefix of the libraries ("Qt4" or "Qt5" usually)
+--
+premake.api.register {
+	name = "qtprefix",
+	scope = "config",
+	kind = "string"
+}
+
+--
+-- Set a suffix for the libraries ("d" usually when using Debug Qt libs)
+--
+premake.api.register {
+	name = "qtsuffix",
+	scope = "config",
+	kind = "string"
+}
+
+--
+-- Specify the modules to use (will handle include paths, links, etc.)
+-- See premake.extensions.qt.modules for a list of available modules.
+--
+premake.api.register {
+	name = "qtmodules",
+	scope = "config",
+	kind = "string-list"
+}
+
+--
+-- Specify the path, relative to the current script, where the files generated
+-- by Qt will be created. If this command is not used, the default behavior
+-- is to generate those files in the objdir.
+--
+premake.api.register {
+	name = "qtgenerateddir",
+	scope = "config",
+	kind = "path"
+}
+
+--
+-- Specify a list of custom options to send to the Qt moc command line.
+--
+premake.api.register {
+	name = "qtmocargs",
+	scope = "config",
+	kind = "string-list"
+}
+
+--
+-- Specify a list of custom options to send to the Qt uic command line.
+--
+premake.api.register {
+	name = "qtuicargs",
+	scope = "config",
+	kind = "string-list"
+}
+
+--
+-- Specify a list of custom options to send to the Qt rcc command line.
+--
+premake.api.register {
+	name = "qtrccargs",
+	scope = "config",
+	kind = "string-list"
+}
+
+--
+-- Private use only : used by the addon to know if qt has already been enabled or not
+--
+premake.api.register {
+	name = "qtenabled",
+	scope = "project",
+	kind = "boolean"
+}
+
+
+--
+-- Always load
+--
+return true
